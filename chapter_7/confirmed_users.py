854b6cc6981fee4eb856a34742dd77f214e2f432
knowreb <148774731+knowreb@users.noreply.github.com>
2024-01-18 21:29:38 +0100

    Update confirmed_users.py

--- chapter_7/confirmed_users.py
+++ chapter_7/confirmed_users.py
@@ -1,17 +1,17 @@
-# Rozpoczynamy od uzytkownikow ktorzy maja byc zweryfikowani
-# tworzymy tez pusta liste przeznaczona do przechowywania zweryfikowanych uzytkownikow
-unconfirmed_users = ['alicja', 'bartek', 'katarzyna',]
+# We start with the users to be verified
+# We also create an empty list to hold verified users
+unconfirmed_users = ['alice', 'bart', 'kate',]
 confirmed_users = []
 
-# weryfikujemy [pszczegolnych uzytkownikow, ddpoki lista nie bedzie pusta.
-# kazdego zweryfikowanego uzytkownika przenosimy na oddzielna liste
+# we verify individual users so that the list is not empty.
+# we move each verified user to a separate list
 while unconfirmed_users:
     current_user = unconfirmed_users.pop()
 
-    print(f"Weryfikacja użytkownika: {current_user.title()}")
+    print(f"User verification: {current_user.title()}")
     confirmed_users.append(current_user)
 
-# wyświetlenie wszystkich zweryfikowanych uzytkownikow
-print("\nZweryfikowano wymienionych poniżej użytkowników:")
+# display of all verified users
+print("\nThe following users were verified:")
 for confirmed_user in confirmed_users:
     print(confirmed_user.title())

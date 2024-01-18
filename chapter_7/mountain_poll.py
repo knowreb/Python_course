d9b9eab0b1e36a862e2687effd22d869bf7a7c33
knowreb <148774731+knowreb@users.noreply.github.com>
2024-01-18 21:34:36 +0100

    Update mountain_poll.py

--- chapter_7/mountain_poll.py
+++ chapter_7/mountain_poll.py
@@ -1,20 +1,20 @@
 responses = {}
 
-# ustawienie flagi wskazujacej, czy ankieta jest aktywna
+# setting a flag to indicate whether a poll is active
 polling_active = True
 while polling_active:
-    # prośba o podanie imienia uczestnika ankiety oraz odpowiedzi na pytanie.
-    name = input("\nJak masz na imię?")
-    response = input("Na który szczyt chciałbyś się wspiąć pewnego dnia?")
+    # requesting the name of the survey participant and the answer to the question.
+    name = input("\nWhat is your name?")
+    response = input("Which peak would you like to climb one day?")
 
-    # umieszczenie odpowiedzi w słowniku:
+    # putting the answer in the dictionary:
     responses[name] = response
-    # ustalenie czy ktokolwiek jeszcze chce wziać udział w ankiecie.
-    repeat = input("Czy ktokolwiek inny chce wziąć udział w ankiecie? (tak / nie)")
-    if repeat == 'nie':
+    # finding out if anyone else wants to take part in the survey.
+    repeat = input("Does anyone else want to take part in the survey? (yes / no)")
+    if repeat == 'no':
         polling_active = False
 
-# ankieta została zakończona i wyświetlamy jej wyniki.
-print("\n--- Wyniki ankiety ---")
+# the survey has been completed and the results are displayed.
+print("\n--- Survey results ---")
 for name, response in responses.items():
-    print(f"{name} chciałby się wspiąć na {response}.")
+    print(f"{name} would like to climb the {response}.")

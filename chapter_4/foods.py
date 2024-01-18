b9e81c73d90e3b728ed3bbac20b97eee1bbae946
knowreb <148774731+knowreb@users.noreply.github.com>
2024-01-18 16:42:23 +0100

    Update foods.py

--- chapter_4/foods.py
+++ chapter_4/foods.py
@@ -1,12 +1,12 @@
-my_foods = ['pizza', 'falafel', 'ciasto z marchwi']
+my_foods = ['pizza', 'falafel', 'brownie']
 friend_foods = my_foods[:]
 
 my_foods.append('cannolo')
-friend_foods.append('lody')
+friend_foods.append('ice cream')
 
-print("Moje ulubione potrawy to:")
+print("My favourite dishes are:")
 print(my_foods)
 
-print("\nUlubione potrawy mojego przyjaciela to:")
+print("\nFavourite dishes of my friend are:")
 print(friend_foods)
 

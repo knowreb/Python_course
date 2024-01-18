50de9ccd358a22f4e05d9036f2366275d8e98f6b
knowreb <148774731+knowreb@users.noreply.github.com>
2024-01-18 21:08:36 +0100

    Update 5.3_5.4_5.5_5.6_5.7.py

--- chapter_5/5.3_5.4_5.5_5.6_5.7.py
+++ chapter_5/5.3_5.4_5.5_5.6_5.7.py
@@ -8,26 +8,85 @@ else:
 
 print('5.4')
 
-alien_color = 'red'
-
+alien_color = 'green'
 if alien_color =='green':
     print('You have earned 5 points.')
 else:
-    print('You don't get 5 for shooting down the alien.')
-    
+    print("You don't get 5 for shooting down the alien.")
+
+print('\n')
+alien_color = 'red'
+if alien_color == 'green':
+   print('You get 5 points for shooting down an alien.')
+else:
+   print("You don't get 5 points for shooting down an alien.")
+
+print('5.5')
+alien_color = 'green':
+if alien_color == 'green':
+   print('You get 5 points for shooting down an alien.')
+elif alien_color == 'yellow':
+   print('You get 10 points for shooting down an alien.')
+else alien_color == 'red':
+   print('You get 15 points for shooting down an alien.')
+
+# v2
+alien_color = 'yellow'
+if alien_color == 'green':
+   print('You get 5 points for shooting down an alien.')
+elif alien_color == 'yellow':
+   print('You get 10 points for shooting down an alien.')
+elif alien_color == 'red':
+   print('You get 15 points for shooting down an alien.')
+else:
+   print("You don't get 5 points!")
+
+# v3
+alien_color = 'red'
+if alien_color == 'green':
+   print('You get 5 points for shooting down an alien.')
+elif alien_color == 'blue':
+   print('You get 10 points for shooting down an alien.')
+elif alien_color == 'red':
+   print('You get 15 points for shooting down an alien.')
+else:
+   print("You don't get 5 points!")
+
+# v4
+alien_color = 'pink'
+if alien_color == 'green':
+   print('You get 5 points for shooting an alien.')
+elif alien_color == 'orange':
+   print('You get 10 points for shooting an alien.')
+elif alien_color == 'yellow':
+   print('You get 15 points for shooting an alien.')
+else:
+   print("You don't get 5 points!")
+   
 print('5.6')
 age = 12
 
 if age <2:
     print('You are an infant.')
 elif age >= 4 and age < 13:
-    print('You are a child.')
+    print('You are child.')
 elif age >= 13 and age < 20:
-    print('You are a teenager.')
+    print('You are teenager.')
 elif age >= 20 and age < 65:
-    print('You are an adult.')
+    print('You are adult.')
 elif age >= 65:
-    print('You are a senior.')
+    print('You are elder.')
 
 print('5.7')
-favourite_fruits = ['mango', 'raspberry', 'banana']
+favourite_fruits = ['mango', 'raspberries', 'bananas']
+if 'mango' in favourite_fruits:
+   print('You really like mango!')
+if 'orange' in favourite_fruits:
+   print('You really like orange!')
+if 'strawberries' in favourite_fruits:
+   print('You really like strawberries!')
+if 'raspberries' in favourite_fruits:
+   print('You really like raspberries!')
+if 'bananas' in favourite_fruits:
+   print("You really like bananas!")
+   

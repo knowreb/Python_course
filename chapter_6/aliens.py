a674eca6b3b1e6215e2435b5c2eac86730f8a57d
knowreb <148774731+knowreb@users.noreply.github.com>
2024-01-18 21:19:34 +0100

    Update aliens.py

--- chapter_6/aliens.py
+++ chapter_6/aliens.py
@@ -1,37 +1,37 @@
-alien_0 = {'color': 'zielony', 'points': 5}
-alien_1 = {'color': 'żółty', 'points': 10}
-alien_2 = {'color': 'czerwony', 'points': 15}
+alien_0 = {'color': 'green', 'points': 5}
+alien_1 = {'color': 'yellow', 'points': 10}
+alien_2 = {'color': 'red', 'points': 15}
 
 aliens = [alien_0, alien_1, alien_2]
 
 for alien in aliens:
     print(alien)
 
-# utworzenie pustej listy przenaczonej do przechowywania obcych
+# create an empty list for storing strangers
 aliens = []
 
-# utworzenie 30 zielonych obcych
+# creation of 30 green aliens
 for alien_number in range(30):
-    new_alien = {'color': 'zielony', 'points': 5, 'speed': 'wolno'}
+    new_alien = {'color': 'green', 'points': 5, 'speed': 'slow'}
     aliens.append(new_alien)
 
-# Wyswietlenie całkowitej liczby utworzonych obcych
-print(f"Całkowita liczba obcych: {len(aliens)}")
+# displaying the total number of strangers created
+print(f"Total number of aliens: {len(aliens)}")
 
-# utworzenie pustej listy przeznaczonej do przechowywania obcych
+# create an empty list for storing strangers
 aliens = []
-# utworzenie 30 zielonych obcych.
+# creation of 30 green aliens.
 for alien_number in range(30):
-    new_alien = {'color':'zielony', 'points': 5, 'speed': 'wolno'}
+    new_alien = {'color':'green', 'points': 5, 'speed': 'slow'}
     aliens.append(new_alien)
 
 for alien in aliens[0:3]:
-    if alien['color'] == 'zielony':
-        alien['color'] = 'żółty'
-        alien['speed'] = ('średnio')
+    if alien['color'] == 'green':
+        alien['color'] = 'yellow'
+        alien['speed'] = 'medium'
         alien['points'] = 10
 
-# wyswietlenie pierwszych pieciu obcych:
+# displaying the first five aliens:
 for alien in aliens[:5]:
     print(alien)
 print('...')

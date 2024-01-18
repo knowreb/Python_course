b2c2f4dcdcf0ef44640d589e8f22dca98df507b3
knowreb <148774731+knowreb@users.noreply.github.com>
2024-01-18 18:14:04 +0100

    Update and rename kolory_obcych53.py to 5.3_5.4_5.5_5.6_5.7.py

---
+++ chapter_5/5.3_5.4_5.5_5.6_5.7.py
@@ -0,0 +1,33 @@
+print('5.3')
+alien_color = 'green'
+
+if alien_color == 'green':
+   print('You have earned 5 points!')
+else:
+   print('No points!')
+
+print('5.4')
+
+alien_color = 'red'
+
+if alien_color =='green':
+    print('You have earned 5 points.')
+else:
+    print('You don't get 5 for shooting down the alien.')
+    
+print('5.6')
+age = 12
+
+if age <2:
+    print('You are an infant.')
+elif age >= 4 and age < 13:
+    print('You are a child.')
+elif age >= 13 and age < 20:
+    print('You are a teenager.')
+elif age >= 20 and age < 65:
+    print('You are an adult.')
+elif age >= 65:
+    print('You are a senior.')
+
+print('5.7')
+favourite_fruits = ['mango', 'raspberry', 'banana']

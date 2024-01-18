ebf9bb4f513fd919aec65a0cab8e25a0a51d40fc
knowreb <148774731+knowreb@users.noreply.github.com>
2024-01-18 21:16:27 +0100

    Update alien.py

--- chapter_6/alien.py
+++ chapter_6/alien.py
@@ -1,31 +1,31 @@
-# alien_0 = {'color': 'zielony', 'points': 5}
-# print(alien_0['color'])
-#
-# alien_0['x_position'] = 0
-# alien_0['y_position'] = 25
-# print(alien_0)
-#
-#
-# alien_0 = {'color': 'zielony'}
-# print(f"Obcy ma kolor {alien_0['color']}.")
-#
-# alien_0['color'] = 'żółty'
-# print(f"Obcy ma teraz kolor {alien_0['color']}.")
+alien_0 = {'color': 'green', 'points': 5}
+print(alien_0['color'])
 
-alien_0 = {'x_position': 0, 'y_position': 25, 'speed': 'średnio'}
-print(f"Początkowa wartość x-position: {alien_0['x_position']}")
+alien_0['x_position'] = 0
+alien_0['y_position'] = 25
+print(alien_0)
 
-#przesunięcie obcego w prawo.
-# Ustalenie odległości, jaką powinien pokonać obcy poruszający się z daną szybkością.
-if alien_0['speed'] == 'wolno':
+
+alien_0 = {'color': 'green'}
+print(f"The alien has a colour {alien_0['color']}.")
+
+alien_0['color'] = 'yellow'
+print(f"The alien has a colour {alien_0['color']}.")
+
+alien_0 = {'x_position': 0, 'y_position': 25, 'speed': 'medium'}
+print(f"Initial value x-position: {alien_0['x_position']}")
+
+# Moving a stranger to the right.
+# Determining the distance to be covered by a stranger moving at a given speed.
+if alien_0['speed'] == 'slow:
     x_increment = 1
-elif alien_0['speed'] == 'średnio':
+elif alien_0['speed'] == 'medium':
     x_increment = 2
 else:
-    # To musi być szybki obcy.
+    #It must be a fast stranger.
     x_increment = 3
 
-# nowe położenie to suma dotychczasowego położenia i wartości x_increment
+# the new position is the sum of the previous position and the x_increment value
 alien_0['x_position'] = alien_0['x_position'] + x_increment
 
-print(f"Nowa wartość x-position: {alien_0['x_position']}")
+print(f"New value x-position: {alien_0['x_position']}")

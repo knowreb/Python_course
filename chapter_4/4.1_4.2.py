5426744b0139e38d9c1aabf2fb73638331d5cf35
knowreb <148774731+knowreb@users.noreply.github.com>
2024-01-22 21:41:27 +0100

    Add files via upload

---
+++ chapter_4/4.1_4.2.py
@@ -0,0 +1,13 @@
+print("4.1")
+pizzas = ['pepperoni', 'neapolitan', 'margherita']
+for pizza in pizzas:
+    print(f"I like {pizza.title()} pizza!")
+
+print("I really love pizza!")
+
+print("4.2")
+animals = ['dog', 'cat', 'horse']
+for animal in animals:
+    print(f'{animal.title()} is a true human friend!')
+
+print("All animals are wonderful!")

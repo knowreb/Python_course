6c94890f98271fa38961510509e8068b8519c796
knowreb <148774731+knowreb@users.noreply.github.com>
2024-01-22 15:45:52 +0100

    Update 3.4_3.5_3.6_3.7.py

--- chapter_3/3.4_3.5_3.6_3.7.py
+++ chapter_3/3.4_3.5_3.6_3.7.py
@@ -6,11 +6,26 @@ print(f'I would like to cordially invite to lunch {visitors[2]}')
 print(f'I would like to cordially invite to lunch {visitors[3]}')
 
 print("3.5")
-print(f'\nUnfortunately, {visitors[3]} cannot come to lunch')
 
 visitors = ['Mary', 'Jane', 'Roma', 'Adrian']
-visitors[3] = 'Roma'
+
+print(f'I would like to cordially invite to lunch {visitors[0]}')
+print(f'I would like to cordially invite to lunch {visitors[1]}')
+print(f'I would like to cordially invite to lunch {visitors[2]}')
 print(f'I would like to cordially invite to lunch {visitors[3]}')
+
+print(f'\nUnfortunately, Roma cannot come to lunch')
+unable_come = 'Roma'
+visitors.remove(unable_come)
+print(visitors)
+visitors.append('Bruno')
+print(visitors)
+print(f'I would like to cordially invite to lunch {visitors[0]}')
+print(f'I would like to cordially invite to lunch {visitors[1]}')
+print(f'I would like to cordially invite to lunch {visitors[2]}')
+print(f'I would like to cordially invite to lunch {visitors[3]}')
+
+print("3.6")
 print('\nFinding a bigger table!')
 
 visitors = ['Mary', 'Jane', 'Roma', 'Adrian']
@@ -28,11 +43,15 @@ print(f'I would like to cordially invite to lunch {visitors[4]}')
 print(f'I would like to cordially invite to lunch {visitors[5]}')
 print(f'I would like to cordially invite to lunch {visitors[6]}')
 
+print("3.7")
 print("\nI'm sorry, unfortunately I can invite only two friend😭")
-
 print(visitors)
 
 popped_visitors = visitors.pop(0)
+print(visitors)
+print(f'\nSee you soon {popped_visitors}!')
+
+popped_visitors = visitors.pop(1)
 print(visitors)
 print(f'\nSee you soon {popped_visitors}!')
 
@@ -44,26 +63,16 @@ popped_visitors = visitors.pop(3)
 print(visitors)
 print(f'\nSee you soon {popped_visitors}!')
 
-popped_visitors = visitors.pop(3)
+popped_visitors = visitors.pop(2)
 print(visitors)
 print(f'\nSee you soon {popped_visitors}!')
 
 print(f'\nCordially invite you {visitors [0]} to lunch!')
 print(f'\nCordially invite you {visitors [1]} to lunch!')
-print(f'\nCordially invite you {visitors [2]} to lunch!')
 
-print(visitors)
+del visitors [0]
 del visitors [0]
 print(visitors)
-
-print(visitors)
-del visitors [0]
-print(visitors)
-
-print(visitors)
-del visitors [0]
-print(visitors)
-
 
 
 
